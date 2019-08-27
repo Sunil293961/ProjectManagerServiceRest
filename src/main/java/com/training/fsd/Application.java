b0fba06c073edab1e1b33a92212b44c9fe864cc6
Sunil293961 <sunil.deshpande@cognizant.com>
2019-08-27 22:24:50 +0530

    FSE Final Project. Rest service

--- src/main/java/com/training/fsd/Application.java
+++ src/main/java/com/training/fsd/Application.java
@@ -7,6 +7,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -28,5 +30,6 @@ public class Application {
 		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter(mapper);
 		return converter;
 	}
+  
 
 }
--- src/main/java/com/training/fsd/model/Project.java
+++ src/main/java/com/training/fsd/model/Project.java
@@ -31,8 +31,8 @@ import javax.persistence.Transient;
 @Entity
 @Table(name = "project")
 @SqlResultSetMapping(name = "TaskSummary", entities = @EntityResult(entityClass = Project.class, fields = {
-		@FieldResult(name = "project_Id", column = "Project_ID"),
-		@FieldResult(name = "project", column = "Project"), @FieldResult(name = "startDate", column = "StartDate"),
+		@FieldResult(name = "projectId", column = "Project_ID"),
+		@FieldResult(name = "projectName", column = "Project"), @FieldResult(name = "startDate", column = "StartDate"),
 		@FieldResult(name = "endDate", column = "EndDate"), @FieldResult(name = "priority", column = "Priority"),
 		@FieldResult(name = "user", column = "User_ID") }),
 		// @FieldResult(name = "tasksCount", column = "tasksCount"),
@@ -47,9 +47,9 @@ public class Project implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="Project_ID")
-	private int project_id;
+	private int projectId;
 	@Column(name="Project")
-	private String project;
+	private String projectName;
 	private Date startDate;
 	private Date endDate;
 	private int priority;
@@ -80,8 +80,8 @@ public class Project implements Serializable {
 	public Project(int project_id, String project, Date startDate, Date endDate, int priority, User user,
 			long tasksCount, long completedTasks) {
 		super();
-		this.project_id = project_id;
-		this.project = project;
+		this.projectId = project_id;
+		this.projectName = project;
 		this.startDate = startDate;
 		this.endDate = endDate;
 		this.priority = priority;
@@ -130,22 +130,20 @@ public class Project implements Serializable {
 		this.user = user;
 	}
 
-	public int getProject_id() {
-		return project_id;
+	public int getProjectId() {
+		return projectId;
 	}
-
-	public void setProject_id(int project_id) {
-		this.project_id = project_id;
+	public void setProjectId(int projectId) {
+		this.projectId = projectId;
 	}
-
-	public String getProject() {
-		return project;
+	
+	
+	public String getProjectName() {
+		return projectName;
 	}
-
-	public void setProject(String project) {
-		this.project = project;
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
 	}
-
 	public int getPriority() {
 		return priority;
 	}
@@ -153,11 +151,12 @@ public class Project implements Serializable {
 	public void setPriority(int priority) {
 		this.priority = priority;
 	}
-
 	@Override
 	public String toString() {
-		return "Project [project_id=" + project_id + ", project=" + project + ", startDate=" + startDate + ", endDate="
-				+ endDate + ", priority=" + priority + ", user=" + user + ", tasksCount=" + tasksCount
+		return "Project [projectId=" + projectId + ", projectName=" + projectName + ", startDate=" + startDate
+				+ ", endDate=" + endDate + ", priority=" + priority + ", user=" + user + ", tasksCount=" + tasksCount
 				+ ", completedTasks=" + completedTasks + "]";
 	}
+
+	
 }
